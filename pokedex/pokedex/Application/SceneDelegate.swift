610e9decf1707675ec5cef7fb81330e42d3827aa
Ana Paula Lesniovski <anap.lesniovski@gmail.com>
2023-08-18 15:14:47 -0300

    configura a navegacao no app utilizando UINavigationController.

--- pokedex/pokedex/Application/SceneDelegate.swift
+++ pokedex/pokedex/Application/SceneDelegate.swift
@@ -14,12 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let safeWindow = UIWindow(windowScene: windowScene)
-        safeWindow.frame = UIScreen.main.bounds
-        safeWindow.rootViewController = PokemonListViewController()
-        safeWindow.makeKeyAndVisible()
+        let rootViewController = PokemonListViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
         
-        window = safeWindow    }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
