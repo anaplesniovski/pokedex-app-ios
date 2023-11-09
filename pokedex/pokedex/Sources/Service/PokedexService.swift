1001c3b9684a9937271e3ac2042dc5b835bde8af
Ana Paula Lesniovski <anap.lesniovski@gmail.com>
2023-11-09 12:13:19 -0300

    refatora service.

---
+++ pokedex/pokedex/Sources/Service/PokedexService.swift
@@ -0,0 +1,8 @@
+//
+//  PokedexService.swift
+//  pokedex
+//
+//  Created by Ana Paula Lesniovski dos Santos on 09/11/23.
+//
+
+import Foundation
---
+++ pokedex/pokedex/Sources/Service/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  pokedex
+//
+//  Created by Ana Paula Lesniovski dos Santos on 09/11/23.
+//
+
+import Foundation
